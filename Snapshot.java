bb319d3e6bcce25783d2e36913fd4dc7d44dba86
cartieraidan <cartieraidan@gmail.com>
2025-12-04 23:28:50 -0500

    data structure for saving game states

---
+++ Snapshot.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+
+/**
+ * Class made for taking snapshot of current game state then adding into a stack for undo and redo options.
+ * And saving and loading games.
+ */
+public class Snapshot implements Serializable {
+    /* Add all objects needed for game snapshot
+    deck
+    discard pile
+    list players
+    round counter
+    game counter
+    direction
+    playerindex
+    drawCard
+    wildDraw
+    wildDrawColour
+     */
+
+}
